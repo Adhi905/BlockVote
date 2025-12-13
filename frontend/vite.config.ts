26444903c84b09df8822aa616873a46a629157bf
Aditya G <baladitya.95@gmail.com>
2025-12-14 02:18:15 +0530

    Deploy to root path

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -9,7 +9,7 @@ export default defineConfig(({ mode }) => ({
     host: "::",
     port: 8080,
   },
-  base: process.env.NODE_ENV === 'production' ? '/blockvote/' : '/',
+
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
   resolve: {
     alias: {
